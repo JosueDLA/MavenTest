70f5fc542d9f8237d743aa3c83513bf4c963066d
Josue <jdeleona10@miumg.edu.gt>
2019-06-05 06:58:25 -0600

    test REST controller with jar

---
+++ test2/src/main/java/gt/edu/umg/ingenieria/sistemas/test2/Main.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SpringApplication.run(Main.class, args);
+    }
+    
+}
